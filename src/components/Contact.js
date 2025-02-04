3f2c3be43402b5108e50340a65d270c28c72f587
niteen-codes <niteenjha190@gmail.com>
2025-02-05 03:28:54 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,7 @@ const Contact = () => {
     const [status, setStatus] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    const API_URL = process.env.REACT_APP_API_URL || 'https://my-app-backend-232q.onrender.com';
+    const API_URL = process.env.REACT_APP_API_URL || 'https://my-app-backend-1-112v.onrender.com';
 
     const handleInputChange = (e) => {
         const { id, value } = e.target;
